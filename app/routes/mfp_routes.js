fac417fc4b94dda9f63b5c198856c1f717d44133
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-09-12 08:51:34 -0700

    ternary magic

--- app/routes/mfp_routes.js
+++ app/routes/mfp_routes.js
@@ -6,9 +6,6 @@ module.exports = function (app, db) {
   app.get('/nutrients', (req, res) => {
     const from = req.query.from || null;
     const to = req.query.to || null;
-
-    if( !from && !to ) { res.send('invalid data') }
-    else if ( !to ) { res.json({ from }); }
-    else { res.json({ from, to }); }
+    ( from && to ) ? res.json({ from, to }) : ( from ) ? res.json({ from }) : res.send('invalid request');
   });
 };
