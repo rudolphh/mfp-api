3308465900cf36908ae1965e0608c1c981cce7d0
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-09-16 09:54:48 -0700

    add not found middleware

--- server.js
+++ server.js
@@ -33,7 +33,12 @@ mfp.apiStatusCheck(function(errors) {
 });
 
 
-
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
 
 //Start our server and tests!
 app.listen(port, function () {
